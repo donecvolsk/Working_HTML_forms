62b700e78d231e96eb61b1584910e1161fbe1740
donecvolsk <donecvolsk@yandex.ru>
2024-04-23 16:12:22 +0400

    Добавление удаления поповера 230424

--- src/js/popover/popover.js
+++ src/js/popover/popover.js
@@ -1,18 +1,17 @@
 export default function Popover(popoverСontentObj) {
-    const container = document.querySelectorAll(".form"); //контейнер с формами
 
+    const container = document.querySelectorAll(".form"); //контейнер с формами 
     const popoverDiv = document.createElement("div"); // создаем div для тела поповера
-
     const popoverTitle = document.createElement("h3"); // создаем заголовок для тела поповера
-
-    const popoverContent = document.createElement("p"); // создаем текст для тела поповера
-
+    const popoverContent = document.createElement("p"); // создаем текст для тела поповера 
+    const body = document.querySelector("body"); 
 
     container.forEach(element => {
+        
         element.addEventListener("focus", (event) => {
             event.preventDefault();
             const  { target }  = event;
-            const formTrg = target.closest(".form"); 
+            const formTrg = target.closest(".form");
 
             popoverTitle.classList.add("popoverTitle");
             popoverTitle.textContent = popoverСontentObj[formTrg.getAttribute('name')].title;        
@@ -26,9 +25,20 @@ export default function Popover(popoverСontentObj) {
                 popoverDiv.remove;
             } else {
                 popoverDiv.classList.add("popover");
-            } 
-            popoverContent.classList.add("popoverText");        
-            formTrg.before(popoverDiv);          
-        }); 
+            }         
+                    
+            formTrg.before(popoverDiv);
+          
+        });
     });
+
+        body.addEventListener("click", (event) => {
+            const element  = event.target;
+
+            if(element.id === "body") {
+                popoverDiv.classList.add("hidden");
+            } else {
+                popoverDiv.classList.remove("hidden");        
+            }    
+        });  
 }
